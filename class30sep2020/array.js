add0ccfa38c2770374172f28ac80cd35c8e6555b
fazzyA <faz.pak@gmail.com>
2020-09-30 12:32:50 +0500

    30 sep class arrays nested if

---
+++ class30sep2020/array.js
@@ -0,0 +1,18 @@
+//arrays starts from zero
+var studentAge = [17, 20, 20, 16,20];
+// JS  push shift unshift
+studentAge.push(30)
+studentAge.push(11)
+studentAge.push(12)
+console.log(studentAge);
+studentAge.pop();
+studentAge.pop();
+console.log(studentAge);
+
+
+
+
+
+
+
+
--- class30sep2020/index.js
+++ class30sep2020/index.js
@@ -1,1 +1,23 @@
-console.log('my new class 30');
+//console.log('my new class 30');
+// age < 18 and age >= 60 and nationality other than usa
+//greencard
+// && and
+// || or
+
+// age < 18 || age >=60 nationality !=usa
+var nationality = 'pak';
+var age = 70;
+     // t || false && true
+if((age < 18 || age >=60) && nationality !='USA') {
+    if(age<18){
+        console.log('greencard holder for kids')
+     
+    }
+    else{
+            console.log('greencard holder')
+
+    }
+} // parent if
+else{
+    console.log('not allowed')
+}
