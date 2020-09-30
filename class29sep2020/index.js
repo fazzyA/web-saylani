f3ca4eafff67ba9449753c2afdf748a2520fd4a8
fazzyA <faz.pak@gmail.com>
2020-09-30 09:31:50 +0500

    30sep class uploaded

---
+++ class29sep2020/index.js
@@ -0,0 +1,69 @@
+//// prompt /////
+
+// var year = +prompt("Enter your birth year");
+// var age = 2020 - year;
+// console.log(`My age is ${age}`);
+// // take name from user and print it
+
+// > greater
+// < less than
+// >=
+// <=
+// !=
+
+var age = prompt('please enter your age');
+if(age == 20) {
+    console.log(`you r 20`)
+}
+else if(age > 20){
+   console.log(` you are greater in age`)
+ 
+}
+else if(age < 20){
+    console.log(` you are younger in age`)
+
+}
+else{
+    console.log('input is not valid')
+}
+
+
+console.log('chalega')
+
+
+
+// var myNumber = 15;
+// var my2Number = myNumber +10; //25
+//  my2Number = myNumber * (10 + 4); // 
+//  var name = "faiza"
+//  var greet = "Good Morning"
+//  var para = "lorem isskajasdkadkljjalkka jkkjfd"
+// var anotherstr ="ksjadkjashd"
+
+
+// //html css js adv js react
+// //php basic laravel
+//  console.log(name+' \n'+greet + '!');
+
+//  //template literal `` back ticks ES6
+ 
+// //  console.log(`my name is faiza and i want to say ${greet} ! 
+// //  and i am teacher 
+// //  who you dont listen to`)
+
+// console.log(5 + 1 + " 2 + 2 is equal to"); 
+// // " 2 + 2 is equal to = " 5  - 1
+// //  2 + 2 is equal to = 22
+
+
+
+
+
+ 
+
+// // var num =2;
+// // num = 3;
+// // console.log(num++); //3
+// // console.log(num+10); //4
+// // ++num pre increment
+// // num ++ post increment
