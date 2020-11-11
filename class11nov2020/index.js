62559133b94447cec9446b1320ed5b98f73dd133
Faiza Aziz Khan <faz.pak@gmail.com>
2020-11-11 13:04:35 +0500

    meal finder
    meal finder

--- class11nov2020/index.js
+++ class11nov2020/index.js
@@ -1,9 +1,11 @@
 /// var obj = {
  //   meals: [{}, {}, {}]
 //}
+var submitForm  = document.getElementById('submit1');
+console.log(submitForm)
 
-
-function searchRecipe(){
+const searchRecipe = (e) => {
+    e.preventDefault();
 var search  =  document.getElementById('search');
 console.log(search.value) 
 var api2 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`
@@ -19,10 +21,14 @@ fetch(api2) // Call the fetch function passing the url of the API as a parameter
     var row  =  document.getElementById('row');
 
     for(let i = 0; i< recipesArr.length; i++){
-    var div = document.createElement('div')
-    div.className = 'box';
+        var div = document.createElement('div')
+        var img = document.createElement('img')
+        img.setAttribute('src',recipesArr[i].strMealThumb)
+        img.className='myImg'
+        div.className = 'box';
     var text = document.createTextNode(recipesArr[i].strMeal)
     div.appendChild(text)
+    div.appendChild(img)
     row.appendChild(div)
 
     }
@@ -37,3 +43,30 @@ fetch(api2) // Call the fetch function passing the url of the API as a parameter
 });
 search.value = '';
 }
+function searchRandom() {
+    var api = 'https://www.themealdb.com/api/json/v1/1/random.php'
+    fetch(api)
+    .then(res=>res.json())
+    .then(data => {
+        console.log(data)
+        var recipeSingle= data.meals
+        for(let i = 0; i< recipeSingle.length; i++){
+            var row  =  document.getElementById('row');
+            var div = document.createElement('div')
+            var content = document.createElement('div')
+            var textcontent = document.createTextNode(recipeSingle[i].strInstructions)
+            content.appendChild(textcontent)
+            var text = document.createTextNode(recipeSingle[i].strMeal)
+            div.appendChild(text)
+             row.appendChild(div)
+             row.appendChild(content)
+        }
+  
+    })
+
+
+
+
+    
+}
+submitForm.addEventListener('submit', searchRecipe);
